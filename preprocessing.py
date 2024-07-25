a91406fa5f214bf6b6160a8298740635c39b6b29
AdamKubiak <kubiakadam64@gmail.com>
2024-07-25 20:08:23 +0200

    final sentiment model

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,9 @@
 import os
-
+import re
 from tqdm import tqdm
 import pandas as pd
-
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 def prepare_imdb_data_csv(folder_name = 'aclImdb'):
 
@@ -24,3 +25,31 @@ def prepare_imdb_data_csv(folder_name = 'aclImdb'):
     df.to_csv('sentiment_imdb_data.csv', index= False, encoding='utf-8')
 
     return df
+
+
+def regex_preprocessing(text):
+    # Usuwanie znaczników HTML
+    text = re.sub('<[^>]*>', '', text)
+    
+    # Wyszukiwanie emotikonów
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|D|P)', text)
+    
+    # Czyszczenie tekstu i konwersja na małe litery
+    text = re.sub('[\W]+', ' ', text.lower())
+    
+    # Łączenie tekstu z emotikonami, usuwanie znaków '-'
+    text = text + ' ' + ' '.join(emoticons).replace('-', '')
+    
+    # Usuwanie podwójnych spacji, jeśli występują
+    text = re.sub('\s+', ' ', text).strip()
+    
+    return text
+
+
+def tokenize(text):
+    return text.split()
+
+def tokenize_porter(text):
+    porter = PorterStemmer()
+    stop_words = stopwords.words('english')
+    return [porter.stem(word) for word in tokenize(text) if word not in stop_words]
